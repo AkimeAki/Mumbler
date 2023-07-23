386073f0d19ad11d51105756a4bf6c5d00e943d5
Rinmy <62400162+AkimeAki@users.noreply.github.com>
2023-07-23 14:16:21 +0900

    ポートを設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
 	server: {
-		port: 4001
+		port: 4001,
+		host: "0.0.0.0"
 	},
 	output: "server",
 	adapter: node({
